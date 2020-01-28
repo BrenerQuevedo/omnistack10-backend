9e38f6d9e6635f4e1407185d1e144d944a67abb0
BrenerQuevedo <39017457+BrenerQuevedo@users.noreply.github.com>
2020-01-28 16:11:24 -0300

    nada demais

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,20 @@
 const {Router} = require("express");
+const axios = require('axios');
+
 
 const routes = Router();
+
+routes.post("/devs", async (req, res) => {
+    const {github_username} = req.body;
+
+    const apiRes = await axios.get(`http://api.github.com/users/${github_username}`);
+
+    console.log(apiRes.data);
+
+    return res.json({message: 'hello brener'});
+
+
+});
 
 
 module.exports = routes;
